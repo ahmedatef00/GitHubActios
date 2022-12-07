c0172df18db6e39204d30c671b50b83f80fc323c
AhmedAtef <ahmedatef62437@gmail.com>
2022-12-07 12:56:09 +0200

    initial commit

---
+++ src/test/java/com/example/GitHubActios/GitHubActiosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GitHubActios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubActiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
