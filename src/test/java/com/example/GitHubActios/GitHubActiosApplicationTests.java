0ebd4286246d0e88f8e8ae4fea7c07286da237a8
AhmedAtef <ahmedatef62437@gmail.com>
2022-12-11 18:44:22 +0200

    edit dockerfile

--- src/test/java/com/example/GitHubActios/GitHubActiosApplicationTests.java
+++ src/test/java/com/example/GitHubActios/GitHubActiosApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.GitHubActios;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = GitHubActiosApplicationTests.class)
 class GitHubActiosApplicationTests {
 
 	@Test
